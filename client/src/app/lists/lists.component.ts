5ee66506755bc8ac5252a42ff5458d9b86840b6c
tbadaczewski <tbadaczewski@gmail.com>
2023-10-08 19:43:33 -0500

    Section 14

--- client/src/app/lists/lists.component.ts
+++ client/src/app/lists/lists.component.ts
@@ -1,15 +1,41 @@
 import { Component, OnInit } from '@angular/core';
+import { Member } from '../models/member';
+import { Pagination } from '../models/Pagination';
+import { MembersService } from '../_services/members.service';
 
 @Component({
   selector: 'app-lists',
   templateUrl: './lists.component.html',
-  styleUrls: ['./lists.component.scss']
+  styleUrls: ['./lists.component.scss'],
 })
 export class ListsComponent implements OnInit {
+  members: Member[] | undefined;
+  predicate: string = 'liked';
+  pageNumber: number = 1;
+  pageSize: number = 5;
+  pagination: Pagination | undefined;
 
-  constructor() { }
+  constructor(private memberService: MembersService) {}
 
   ngOnInit(): void {
+    this.loadLikes();
   }
 
+  loadLikes() {
+    this.memberService
+      .getLikes(this.predicate, this.pageNumber, this.pageSize)
+      .subscribe({
+        next: (response) => {
+          this.members = response.result;
+          this.pagination = response.pagination;
+        },
+      });
+  }
+
+  pageChanged(event: any) {
+    if (this.pageNumber != event.page) {
+      this.pageNumber = event.page;
+      this.loadLikes();
+    }
+  }
 }
